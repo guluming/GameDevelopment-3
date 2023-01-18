4dedfbd2666dc4f789ad1186a32b32619f255c24
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-01-18 23:20:21 +0900

    강아지 오브젝트 및 먹이 오브젝트 스크립트 추가 - 강아지 오브젝트는 마우스를 따라서 움직임    ㄴ 단, x축으로만 움직이고 fishshop을 벗어나지 않음 - 먹이 오브젝트 발사    ㄴ 강아지 오브젝트 머리 위에서 발사    ㄴ 벽에 부딪힐 경우 오브젝트 삭제

---
+++ dogvscat/Assets/Scripts/gameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject dog;
+    public GameObject food;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("makeFood", 0.0f, 0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void makeFood() 
+    {
+        float x = dog.transform.position.x;
+        float y = dog.transform.position.y + 2;
+        Instantiate(food, new Vector3(x,y,0), Quaternion.identity);
+    }
+}
